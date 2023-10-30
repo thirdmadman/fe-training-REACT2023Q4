124d95987a0f67b2c131c948c928056b69ed119c
thirdmadman <thirdmadman@gmail.com>
2023-10-30 20:18:36 +0100

    refactor: lint document feat: add image download

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,19 +3,32 @@ import { ICardData } from '../interfaces/ICardData';
 
 export class Card extends React.Component<ICardData> {
   render() {
-    const { title, artistDisplay, artworkTypeTitle, dateDisplay, imageUrl, imagePlaceholder } =
-      this.props;
+    const {
+      title,
+      artistDisplay,
+      artworkTypeTitle,
+      dateDisplay,
+      imageUrl,
+      imagePlaceholder,
+    } = this.props;
 
     return (
       <div className="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
         <div
           className="h-56 w-full bg-cover overflow-hidden relative"
-          style={imagePlaceholder ? {
-            backgroundImage: `url("${imagePlaceholder}")`,
-          } : undefined}
+          style={
+            imagePlaceholder
+              ? {
+                  backgroundImage: `url("${imagePlaceholder}")`,
+                }
+              : undefined
+          }
         >
-          <img className='' src={imageUrl} alt={artistDisplay}/>
-          <button className="absolute right-3 top-3 p-2 rounded-full bg-blue-600 text-white hover:bg-blue-500 focus:outline-none focus:bg-blue-500">
+          <img className="" src={imageUrl} alt={artistDisplay} />
+          <button
+            className="absolute right-3 top-3 p-2 rounded-full bg-blue-600 text-white hover:bg-blue-500 focus:outline-none focus:bg-blue-500"
+            onClick={() => window.open(imageUrl, '_blank')}
+          >
             <svg
               className="h-5 w-5 m-1"
               fill="none"
@@ -25,32 +38,12 @@ export class Card extends React.Component<ICardData> {
               viewBox="0 0 21 21"
               stroke="currentColor"
             >
-              <line
-                x1="16.433"
-                y1="6.25"
-                x2="10.433"
-                y2="16.6423"
-              />
-              <line
-                x1="10.567"
-                y1="16.6423"
-                x2="4.56699"
-                y2="6.25"
-              />
-              <line
-                x1="10.5"
-                y1="3.27835e-08"
-                x2="10.5"
-                y2="16"
-              />
-              <line
-                x1="21"
-                y1="20.5"
-                x2="-4.99559e-08"
-                y2="20.5"
-              />
+              <line x1="16.433" y1="6.25" x2="10.433" y2="16.6423" />
+              <line x1="10.567" y1="16.6423" x2="4.56699" y2="6.25" />
+              <line x1="10.5" y1="3.27835e-08" x2="10.5" y2="16" />
+              <line x1="21" y1="20.5" x2="-4.99559e-08" y2="20.5" />
               <line x1="0.5" y1="21" x2="0.5" y2="15" />
-              <line x1="20.5" y1="21" x2="20.5" y2="15"/>
+              <line x1="20.5" y1="21" x2="20.5" y2="15" />
             </svg>
           </button>
         </div>
