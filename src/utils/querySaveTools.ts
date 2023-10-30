a84e01f464db2bf7b8d2ee6014ba46435987a45d
thirdmadman <thirdmadman@gmail.com>
2023-10-30 18:38:04 +0100

    feat: add tools

---
+++ src/utils/querySaveTools.ts
@@ -0,0 +1,13 @@
+import DataLocalStorageProvider from "../services/DataLocalStorageProvider";
+
+export function saveQueryToLocalStorage(query: string | null) {
+  const localStorageState = DataLocalStorageProvider.getData();
+  localStorageState.userConfigs.lastSearchQuery = query;
+  
+  DataLocalStorageProvider.setData(localStorageState);
+}
+
+export function getQueryFormLocalStorage() {
+  const localStorageState = DataLocalStorageProvider.getData();
+  return localStorageState.userConfigs.lastSearchQuery;
+}
