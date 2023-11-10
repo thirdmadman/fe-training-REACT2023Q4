f428aad47e07d78c16ed957d7e57c51243abe252
thirdmadman <thirdmadman@gmail.com>
2023-11-10 18:58:46 +0100

    feat: update ver of LS

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ export const API_NAME = 'Art Institute of Chicago';
 export const API_URL = 'https://api.artic.edu/api/v1/artworks';
 export const APP_TITLE = `thirdmadman's ${API_NAME} page`;
 
-export const LOCAL_STORAGE_CONFIGS_VERSION = 1;
+export const LOCAL_STORAGE_CONFIGS_VERSION = 2;
 export const LOCAL_STORAGE_ITEM_NAME = 'thirdmadman-art-gallery';
 
 export const ROUTE_MAIN = '/';
