14471b7353a910bc8a0c5a972b4998308ee492be
thirdmadman <thirdmadman@gmail.com>
2023-11-10 18:04:09 +0100

    feat: add ErrorCard

---
+++ src/components/ErrorCard.tsx
@@ -0,0 +1,8 @@
+export const ErrorCard = (title: string, subtitle: string) => (
+  <div className="flex min-w-full py-10 items-center justify-center">
+    <div className="rounded-lg bg-white p-8 text-center shadow-xl">
+      <h1 className="mb-4 text-2xl font-bold">{title}</h1>
+      <p className="text-gray-600">{subtitle}</p>
+    </div>
+  </div>
+);
