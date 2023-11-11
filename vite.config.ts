5209bd4ee8bce2534594db9b696cdbd7cf413a19
thirdmadman <thirdmadman@gmail.com>
2023-11-11 14:12:22 +0100

    chore: update packages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import eslint from 'vite-plugin-eslint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [react(), process.env.mode !== 'test' && eslint()],
   test: {
     globals: true,
     environment: 'jsdom',
