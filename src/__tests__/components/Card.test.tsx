c24a52c742340522c74838a3b529274d76d89b71
thirdmadman <thirdmadman@gmail.com>
2023-11-19 14:06:03 +0100

    feat: update card tests

--- src/__tests__/components/Card.test.tsx
+++ src/__tests__/components/Card.test.tsx
@@ -1,8 +1,21 @@
 import { describe, expect } from 'vitest';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import { Card } from '../../components/Card';
 import { cardDataMock } from '../mocks/cardDataMock';
 import { renderWithProviders } from '../utils/test-utils';
+import { openDetails } from '../../redux/features/detailsSlice';
+
+const dispatchMock = vi.fn(() => {});
+
+const mocks = vi.hoisted(() => {
+  return {
+    useGetOneArtQuery: vi.fn(() => ({})),
+    useAppSelector: vi.fn(() => () => ({})),
+    useAppDispatch: vi.fn(() => dispatchMock),
+  };
+});
+
+vi.mock('../../redux/hooks', () => vi.importActual('../../redux/hooks'));
 
 describe('Card test', () => {
   it('renders without crashing', () => {
@@ -29,5 +42,26 @@ describe('Card test', () => {
     }
 
     expect(container.firstChild).not.toBeNull();
+  });
+
+  it('should call open details action with specific id on card click', () => {
+    vi.mock('../../redux/hooks', () => ({
+      useAppSelector: mocks.useAppSelector,
+      useAppDispatch: mocks.useAppDispatch,
+    }));
+
+    const { container } = renderWithProviders(<Card {...cardDataMock} />);
+
+    const contentDiv = container.firstElementChild;
+
+    if (!contentDiv) {
+      expect(contentDiv).not.toBeNull();
+      return;
+    }
+
+    fireEvent.click(contentDiv);
+
+    expect(dispatchMock).toBeCalledTimes(1);
+    expect(dispatchMock).toBeCalledWith(openDetails(cardDataMock.id));
   });
 });
