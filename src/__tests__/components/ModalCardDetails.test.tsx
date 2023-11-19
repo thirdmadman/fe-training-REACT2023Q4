366e44e37db337db490969dd4756264c3fe232b3
thirdmadman <thirdmadman@gmail.com>
2023-11-19 14:05:09 +0100

    feat: add card details tests

--- src/__tests__/components/ModalCardDetails.test.tsx
+++ src/__tests__/components/ModalCardDetails.test.tsx
@@ -1,10 +1,88 @@
 import { describe, expect } from 'vitest';
 import { ModalCardDetails } from '../../components/ModalCardDetails';
-import { renderWithProviders } from '../utils/test-utils';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { closeDetails } from '../../redux/features/detailsSlice';
+
+const dispatchMock = vi.fn(() => {});
+
+const mocks = vi.hoisted(() => {
+  return {
+    useGetOneArtQuery: vi.fn(() => ({})),
+    useAppSelector: vi.fn(() => () => ({})),
+    useAppDispatch: vi.fn(() => dispatchMock),
+  };
+});
+
+vi.mock('../../redux/api/apiSlice', () => {
+  return {
+    useGetOneArtQuery: mocks.useGetOneArtQuery,
+  };
+});
+
+vi.mock('../../redux/hooks', () => ({
+  useAppSelector: mocks.useAppSelector,
+  useAppDispatch: mocks.useAppDispatch,
+}));
 
 describe('ModalCardDetails test', () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
   test('should render component without crash', async () => {
-    const { container } = renderWithProviders(<ModalCardDetails />);
+    const { container } = render(<ModalCardDetails />);
     expect(container.childElementCount).not.toBe(0);
+  });
+
+  test('should render error when API error', async () => {
+    mocks.useGetOneArtQuery.mockImplementationOnce(
+      vi.fn(() => ({
+        data: null,
+        isError: true,
+        isFetching: false,
+      }))
+    );
+
+    render(<ModalCardDetails />);
+    expect(screen.getByText('Server error')).not.toBeNull();
+  });
+
+  test('should render loading spinner', async () => {
+    mocks.useGetOneArtQuery.mockImplementationOnce(
+      vi.fn(() => ({
+        data: null,
+        isError: false,
+        isFetching: true,
+      }))
+    );
+
+    render(<ModalCardDetails />);
+    expect(screen.getByText('Loading...')).not.toBeNull();
+  });
+
+  test('should render error of not found details', async () => {
+    mocks.useGetOneArtQuery.mockImplementationOnce(
+      vi.fn(() => ({
+        data: null,
+        isError: false,
+        isFetching: false,
+      }))
+    );
+
+    render(<ModalCardDetails />);
+    expect(screen.getByText('No such item exist')).not.toBeNull();
+  });
+
+  test('should call close action on close button click', async () => {
+    render(<ModalCardDetails />);
+
+    const closeButton = screen.getByText('Close details');
+
+    expect(closeButton).not.toBeNull();
+
+    fireEvent.click(closeButton);
+
+    expect(dispatchMock).toBeCalledTimes(1);
+    expect(dispatchMock).toBeCalledWith(closeDetails());
   });
 });
