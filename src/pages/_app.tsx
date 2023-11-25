42a85c1530e96a623b6b6f9160022b74455ea2d8
thirdmadman <thirdmadman@gmail.com>
2023-11-25 11:23:03 +0100

    feat: update _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
+import { wrapper } from '@/redux/store'
 import type { AppProps } from 'next/app'
+import { FC } from 'react';
+import { Provider } from 'react-redux';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import '@/styles/globals.css'
+
+const App: FC<AppProps> = ({ Component, ...rest }) => {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  const { pageProps } = props;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
+};
+
+export default App;
