e09c59e584daa59e72e42c9e69f64aab932e2d8e
thirdmadman <thirdmadman@gmail.com>
2023-11-25 11:21:47 +0100

    feat: move layout

---
+++ src/components/shared/PageLayout.tsx
@@ -0,0 +1,5 @@
+import type { ReactElement } from 'react';
+
+export function PageLayout({children}: {children: ReactElement}) {
+  return <div className="bg-white min-h-screen relative">{children}</div>;
+}
