1759c3e186a21b73c37103280612f7265305485d
thirdmadman <thirdmadman@gmail.com>
2023-12-02 23:54:17 +0100

    feat: impl use of formDataSchema

--- src/utils/validateFormData.ts
+++ src/utils/validateFormData.ts
@@ -1,55 +1,10 @@
-import { boolean, number, object, ref, string, ValidationError } from 'yup';
+import { ValidationError } from 'yup';
 import { IFormDataOptional } from '../interfaces/IFormData';
+import { formDataSchema } from './formDataSchema';
 
 export async function validateFormData(dataToValidate: IFormDataOptional) {
-  const userSchema = object({
-    name: string().label('Name').required(),
-    age: number().label('Age').required().positive().integer(),
-    gender: string().label('Gender').required(),
-    country: string().label('Country').required(),
-    email: string().label('Email').required().email(),
-    password: string()
-      .label('Password')
-      .required()
-      .test({
-        name: 'max',
-        message: '${path} must contain at least 1 character',
-        test: (value) => /^(?=.*[a-z]).+/.test(value),
-      })
-      .test({
-        name: 'max',
-        message: '${path} must contain at least 1 uppercase character',
-        test: (value) => /^(?=.*[A-Z]).+/.test(value),
-      })
-      .test({
-        name: 'max',
-        message: '${path} must contain at least 1 digit',
-        test: (value) => /^(?=.*[0-9]).+/.test(value),
-      })
-      .test({
-        name: 'max',
-        message: '${path} must contain at least 1 special character',
-        test: (value) => /^(?=.*[\W]).+/.test(value),
-      }),
-    passwordRepeat: string()
-      .label('Password')
-      .required()
-      .oneOf([ref('password')], 'Passwords must match'),
-    acceptTC: boolean()
-      .label('Accept of T&C')
-      .required()
-      .isTrue('You must accept T&C'),
-    userPicture: string()
-      .label('User picture')
-      .required()
-      .test({
-        message: '${path} must be png or jpg',
-        test: (value) => /^(?=.*image\/(png|jpg|jpeg);base64).+/.test(value),
-      }),
-  });
-
   try {
-    const formData = await userSchema.validate(dataToValidate, {
+    const formData = await formDataSchema.validate(dataToValidate, {
       abortEarly: false,
     });
     return { formData };
