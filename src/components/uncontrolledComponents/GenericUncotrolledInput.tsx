c0e4962cf37098c424c95188d688a6bdca5710d4
thirdmadman <thirdmadman@gmail.com>
2023-12-02 19:15:32 +0100

    feat: correct padding

--- src/components/uncontrolledComponents/GenericUncotrolledInput.tsx
+++ src/components/uncontrolledComponents/GenericUncotrolledInput.tsx
@@ -38,7 +38,7 @@ export function GenericUncontrolledInput({
         {label}
         <input
           id={id}
-          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="bg-gray-50 mt-4 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           type={type}
           placeholder={placeholder}
           ref={inputRef}
