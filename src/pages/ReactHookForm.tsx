89af69436d5a1593258472614e8db3ed024fe864
thirdmadman <thirdmadman@gmail.com>
2023-12-03 21:58:43 +0100

    feat: update page layout

--- src/pages/ReactHookForm.tsx
+++ src/pages/ReactHookForm.tsx
@@ -5,12 +5,15 @@ import { GenericInput } from '../components/hookComponents/GenericInput';
 import { GenericCheckBoxInput } from '../components/hookComponents/GenericCheckBoxInput';
 import { GenericPasswordInput } from '../components/hookComponents/GenericPasswordInput';
 import { GenericTextInputAutocomplete } from '../components/hookComponents/GenericTextInputAutocomplete';
-import { COUNTRIES } from '../constants';
+import { COUNTRIES, ROUTE_MAIN } from '../constants';
 import { GenericGenderInput } from '../components/hookComponents/GenericGenderInput';
 import { IFormData } from '../interfaces/IFormData';
 import { saveFormData } from '../redux/features/mainPageSlice';
 import { useAppDispatch } from '../redux/hooks';
 import { convertFileToBase64 } from '../utils/convertFileToBase64';
+import { Footer } from '../components/shared/Footer';
+import { Header } from '../components/shared/Header';
+import { Link } from 'react-router-dom';
 
 export function ReactHookForm() {
   const dispatch = useAppDispatch();
@@ -51,119 +54,129 @@ export function ReactHookForm() {
 
   return (
     <>
-      <div>ReactHookForm</div>
-      <form className="max-w-sm mx-auto" onSubmit={handleSubmit(onSubmitEvent)}>
-        <GenericInput<'email'>
-          useFormRegisterReturn={register('email')}
-          error={errors?.email?.message}
-          id="email"
-          type="email"
-          label="Input your email"
-          placeholder="someone@example.com"
-        />
-
-        <GenericPasswordInput<'password'>
-          useFormRegisterReturn={register('password')}
-          error={errors?.password?.message}
-          id="password"
-          type="password"
-          label="Input your password"
-        />
-
-        <GenericInput<'passwordRepeat'>
-          useFormRegisterReturn={register('passwordRepeat')}
-          error={errors?.passwordRepeat?.message}
-          id="password-repeat"
-          type="password"
-          label="Repeat your password"
-        />
-
-        <GenericInput<'name'>
-          useFormRegisterReturn={register('name')}
-          error={errors?.name?.message}
-          id="name"
-          type="text"
-          label="Input your name"
-          placeholder="Mark"
-        />
-
-        <GenericInput<'age'>
-          useFormRegisterReturn={register('age')}
-          error={errors?.age?.message}
-          id="age"
-          type="text"
-          label="Input your age"
-          placeholder="18"
-        />
-
-        <GenericGenderInput<'gender'>
-          useFormRegisterReturn={register('gender')}
-          error={errors?.gender?.message}
-          id="gender"
-          label="Select your gender"
-          placeholder="Input your gender"
-        />
-
-        <GenericTextInputAutocomplete<'country'>
-          useFormRegisterReturn={register('country')}
-          label="Choose your Country"
-          error={errors?.country?.message}
-          id="country"
-          options={COUNTRIES.map((el) => el.name)}
-        />
-
-        <div className="mb-5">
-          <label
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            htmlFor="user_avatar"
+      <Header />
+      <main className="my-8">
+        <div className="container mx-auto px-6">
+          <div>ReactHookForm</div>
+          <form
+            className="max-w-sm mx-auto"
+            onSubmit={handleSubmit(onSubmitEvent)}
           >
-            Upload file
-          </label>
-          <input
-            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            aria-describedby="user_avatar_help"
-            id="user_avatar"
-            type="file"
-            accept=".png,.jpg,.jpeg"
-            onChange={(e) => processUserPicture(e.currentTarget.files)}
-          />
-          <p className="mt-2 text-sm text-red-600 dark:text-red-500">
-            {errors?.userPicture?.message}
-          </p>
-          <div
-            className="mt-1 text-sm text-gray-500 dark:text-gray-300"
-            id="user_avatar_help"
-          >
-            A profile picture is useful to confirm your are logged into your
-            account
-          </div>
-        </div>
+            <GenericInput<'email'>
+              useFormRegisterReturn={register('email')}
+              error={errors?.email?.message}
+              id="email"
+              type="email"
+              label="Input your email"
+              placeholder="someone@example.com"
+            />
 
-        <GenericCheckBoxInput<'acceptTC'>
-          useFormRegisterReturn={register('acceptTC')}
-          label={
-            <>
-              I agree to the{' '}
-              <a
-                href="#"
-                className="text-blue-600 hover:underline dark:text-blue-500"
+            <GenericPasswordInput<'password'>
+              useFormRegisterReturn={register('password')}
+              error={errors?.password?.message}
+              id="password"
+              type="password"
+              label="Input your password"
+            />
+
+            <GenericInput<'passwordRepeat'>
+              useFormRegisterReturn={register('passwordRepeat')}
+              error={errors?.passwordRepeat?.message}
+              id="password-repeat"
+              type="password"
+              label="Repeat your password"
+            />
+
+            <GenericInput<'name'>
+              useFormRegisterReturn={register('name')}
+              error={errors?.name?.message}
+              id="name"
+              type="text"
+              label="Input your name"
+              placeholder="Mark"
+            />
+
+            <GenericInput<'age'>
+              useFormRegisterReturn={register('age')}
+              error={errors?.age?.message}
+              id="age"
+              type="text"
+              label="Input your age"
+              placeholder="18"
+            />
+
+            <GenericGenderInput<'gender'>
+              useFormRegisterReturn={register('gender')}
+              error={errors?.gender?.message}
+              id="gender"
+              label="Select your gender"
+              placeholder="Input your gender"
+            />
+
+            <GenericTextInputAutocomplete<'country'>
+              useFormRegisterReturn={register('country')}
+              label="Choose your Country"
+              error={errors?.country?.message}
+              id="country"
+              options={COUNTRIES.map((el) => el.name)}
+            />
+
+            <div className="mb-5">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="user_avatar"
               >
-                terms and conditions
-              </a>
-              .
-            </>
-          }
-          error={errors?.acceptTC?.message}
-          id="acceptTC"
-        />
+                Upload file
+              </label>
+              <input
+                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                aria-describedby="user_avatar_help"
+                id="user_avatar"
+                type="file"
+                accept=".png,.jpg,.jpeg"
+                onChange={(e) => processUserPicture(e.currentTarget.files)}
+              />
+              <p className="mt-2 text-sm text-red-600 dark:text-red-500">
+                {errors?.userPicture?.message}
+              </p>
+              <div
+                className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                id="user_avatar_help"
+              >
+                A profile picture is useful to confirm your are logged into your
+                account
+              </div>
+            </div>
 
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
-      </form>
+            <GenericCheckBoxInput<'acceptTC'>
+              useFormRegisterReturn={register('acceptTC')}
+              label={
+                <>
+                  I agree to the{' '}
+                  <a
+                    href="#"
+                    className="text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    terms and conditions
+                  </a>
+                  .
+                </>
+              }
+              error={errors?.acceptTC?.message}
+              id="acceptTC"
+            />
+
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </main>
+      <Link to={`${ROUTE_MAIN}`}>Main page</Link>
+      <Footer />
     </>
   );
 }
--- src/pages/UncontrolledForm.tsx
+++ src/pages/UncontrolledForm.tsx
@@ -8,8 +8,11 @@ import { GenderInput } from '../components/uncontrolledComponents/GenderInput';
 import { CheckBoxInput } from '../components/uncontrolledComponents/CheckBoxInput';
 import { TextInputAutocomplete } from '../components/uncontrolledComponents/TextInputAutocomplete';
 import { IFormDataOptional } from '../interfaces/IFormData';
-import { COUNTRIES } from '../constants';
+import { COUNTRIES, ROUTE_MAIN } from '../constants';
 import { PasswordInput } from '../components/uncontrolledComponents/PasswordInput';
+import { Header } from '../components/shared/Header';
+import { Footer } from '../components/shared/Footer';
+import { Link } from 'react-router-dom';
 
 type TFormInputErrors = Partial<Record<keyof IFormDataOptional, Array<string>>>;
 
@@ -93,125 +96,132 @@ export function UncontrolledForm() {
   };
 
   return (
-    <div>
-      <h2>UncontrolledForm</h2>
-      <form
-        className="max-w-sm mx-auto"
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmitEvent();
-        }}
-      >
-        <GenericUncontrolledInput
-          errors={errors?.email}
-          id="email"
-          type="email"
-          label="Input your email"
-          placeholder="someone@example.com"
-          inputRef={emailInput}
-        />
-
-        <PasswordInput
-          errors={errors?.password}
-          id="password"
-          type="password"
-          label="Input your password"
-          inputRef={passwordInput}
-        />
-
-        <GenericUncontrolledInput
-          errors={errors?.passwordRepeat}
-          id="password-repeat"
-          type="password"
-          label="Repeat your password"
-          inputRef={passwordRepeatInput}
-        />
-
-        <GenericUncontrolledInput
-          errors={errors?.name}
-          id="name"
-          type="text"
-          label="Input your name"
-          placeholder="Mark"
-          inputRef={nameInput}
-        />
-
-        <GenericUncontrolledInput
-          errors={errors?.age}
-          id="age"
-          type="text"
-          label="Input your age"
-          placeholder="18"
-          inputRef={ageInput}
-        />
-
-        <GenderInput
-          errors={errors?.gender}
-          id="gender"
-          label="Select your gender"
-          placeholder="Input your gender"
-          inputRef={genderInput}
-        />
-
-        <TextInputAutocomplete
-          label="Choose your Country"
-          errors={errors?.country}
-          id="country"
-          options={COUNTRIES.map((el) => el.name)}
-          inputRef={countryInput}
-        />
-
-        <div className="mb-5">
-          <label
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            htmlFor="user_avatar"
+    <>
+      <Header />
+      <main className="my-8">
+        <div className="container mx-auto px-6">
+          <h2>UncontrolledForm</h2>
+          <form
+            className="max-w-sm mx-auto"
+            onSubmit={(e) => {
+              e.preventDefault();
+              onSubmitEvent();
+            }}
           >
-            Upload file
-          </label>
-          <input
-            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-            aria-describedby="user_avatar_help"
-            id="user_avatar"
-            type="file"
-            ref={userPictureInput}
-            accept=".png,.jpg,.jpeg"
-            onChange={(e) => console.log(e.currentTarget.files)}
-          />
-          {extractErrors(errors?.userPicture)}
-          <div
-            className="mt-1 text-sm text-gray-500 dark:text-gray-300"
-            id="user_avatar_help"
-          >
-            A profile picture is useful to confirm your are logged into your
-            account
-          </div>
-        </div>
+            <GenericUncontrolledInput
+              errors={errors?.email}
+              id="email"
+              type="email"
+              label="Input your email"
+              placeholder="someone@example.com"
+              inputRef={emailInput}
+            />
 
-        <CheckBoxInput
-          label={
-            <>
-              I agree to the{' '}
-              <a
-                href="#"
-                className="text-blue-600 hover:underline dark:text-blue-500"
+            <PasswordInput
+              errors={errors?.password}
+              id="password"
+              type="password"
+              label="Input your password"
+              inputRef={passwordInput}
+            />
+
+            <GenericUncontrolledInput
+              errors={errors?.passwordRepeat}
+              id="password-repeat"
+              type="password"
+              label="Repeat your password"
+              inputRef={passwordRepeatInput}
+            />
+
+            <GenericUncontrolledInput
+              errors={errors?.name}
+              id="name"
+              type="text"
+              label="Input your name"
+              placeholder="Mark"
+              inputRef={nameInput}
+            />
+
+            <GenericUncontrolledInput
+              errors={errors?.age}
+              id="age"
+              type="text"
+              label="Input your age"
+              placeholder="18"
+              inputRef={ageInput}
+            />
+
+            <GenderInput
+              errors={errors?.gender}
+              id="gender"
+              label="Select your gender"
+              placeholder="Input your gender"
+              inputRef={genderInput}
+            />
+
+            <TextInputAutocomplete
+              label="Choose your Country"
+              errors={errors?.country}
+              id="country"
+              options={COUNTRIES.map((el) => el.name)}
+              inputRef={countryInput}
+            />
+
+            <div className="mb-5">
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="user_avatar"
               >
-                terms and conditions
-              </a>
-              .
-            </>
-          }
-          errors={errors?.acceptTC}
-          id="acceptTC"
-          inputRef={acceptTCRepeatInput}
-        />
+                Upload file
+              </label>
+              <input
+                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                aria-describedby="user_avatar_help"
+                id="user_avatar"
+                type="file"
+                ref={userPictureInput}
+                accept=".png,.jpg,.jpeg"
+                onChange={(e) => console.log(e.currentTarget.files)}
+              />
+              {extractErrors(errors?.userPicture)}
+              <div
+                className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                id="user_avatar_help"
+              >
+                A profile picture is useful to confirm your are logged into your
+                account
+              </div>
+            </div>
 
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+            <CheckBoxInput
+              label={
+                <>
+                  I agree to the{' '}
+                  <a
+                    href="#"
+                    className="text-blue-600 hover:underline dark:text-blue-500"
+                  >
+                    terms and conditions
+                  </a>
+                  .
+                </>
+              }
+              errors={errors?.acceptTC}
+              id="acceptTC"
+              inputRef={acceptTCRepeatInput}
+            />
+
+            <button
+              type="submit"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </main>
+      <Link to={`${ROUTE_MAIN}`}>Main page</Link>
+      <Footer />
+    </>
   );
 }
